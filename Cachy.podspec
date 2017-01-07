560672e3004fc15ba58df45d091dd34e594e326f
FedeGens <f.gentile@fingerlinks.org>
2017-01-07 13:54:44 +0100

    First Commit

---
+++ Cachy.podspec
@@ -0,0 +1,34 @@
+Pod::Spec.new do |s|
+
+# 1
+s.platform = :ios
+s.ios.deployment_target = '8.0'
+s.name = "Cachy"
+s.summary = "Cachy is an image caching library."
+s.requires_arc = true
+
+# 2
+s.version = "0.0.1"
+
+# 3
+s.license = { :type => "MIT", :file => "LICENSE" }
+
+# 4 - Replace with your name and e-mail address
+s.author = { "Federico Gentile" => "fgentile95dev@icloud.com" }
+
+
+# 5 - Replace this URL with your own Github page's URL (from the address bar)
+s.homepage = "https://github.com/FedeGens/Cachy"
+
+
+# 6 - Replace this URL with your own Git URL from "Quick Setup"
+s.source = { :git => "https://github.com/FedeGens/Cachy.git", :tag => "#{s.version}"}
+
+
+# 7
+s.framework = "UIKit"
+
+# 8
+s.source_files = "Cachy/*.{swift}"
+
+end
---
+++ CachyDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Cachy
+//
+//  Created by Federico Gentile on 28/12/16.
+//  Copyright © 2016 Federico Gentile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imageView_: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        imageView_.contentMode = .scaleAspectFill
+        imageView_.clipsToBounds = true
+        imageView_.cachyImageFrom(link: "http://www.w3schools.com/css/trolltunga.jpg")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
